1503001a80580ecddb8bcaa2ee74ee9f1d2fe749
Frqstbite <40967034+Frqstbite@users.noreply.github.com>
2022-05-11 22:54:06 -0400

    Functions as standalone script currently

--- main.lua
+++ main.lua
@@ -1,4 +1,32 @@
-function init(plugin)
+local sprite = app.activeSprite
+
+if not sprite then
+    return
+end
+
+local selection = sprite.selection
+
+if sprite.selection.isEmpty then
+    return
+end
+
+local origin = selection.origin
+local cell = app.activeCel --ridiculous spelling corrected
+local image = cell.image
+
+local target = Point(origin.x - cell.position.x, origin.y - cell.position.y)
+local pixelValue = image:getPixel(target.x, target.y)
+local color = Color(pixelValue)
+
+app.fgColor = Color({
+    r = origin.x,
+    g = (sprite.height - 1 - origin.y), --Coordinates start at bottom because Unity is fat; subtraction is necessary for some reason
+    z = 0, --Doesn't really matter yet
+    a = color.alpha
+})
+
+
+--[[ for later :) function init(plugin)
     print("aseprite starting my f")
 
     plugin:newCommand({
@@ -16,4 +44,4 @@ end
 
 function exit(plugin)
     print("exiting")
-end
+end]]
